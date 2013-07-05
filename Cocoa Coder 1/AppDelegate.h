c7b46da52d35b539fbf28b4653b22b6e20229dff
Rob Mayoff <git@rob.dqd.com>
2013-07-05 17:01:20 -0500

    Initial Commit

---
+++ Cocoa Coder 1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Cocoa Coder 1
+//
+//  Created by Rob Mayoff on 7/5/13.
+//  Copyright (c) 2013 Rob Mayoff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Cocoa Coder 1Tests/Cocoa_Coder_1Tests.m
@@ -0,0 +1,36 @@
+//
+//  Cocoa_Coder_1Tests.m
+//  Cocoa Coder 1Tests
+//
+//  Created by Rob Mayoff on 7/5/13.
+//  Copyright (c) 2013 Rob Mayoff. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Cocoa_Coder_1Tests : XCTestCase
+
+@end
+
+@implementation Cocoa_Coder_1Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
